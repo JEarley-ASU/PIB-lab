1d0b344cf5e24c577247e6397a80d957e45d9f9b
JEarley-ASU <justin.earley@asu.edu>
2025-09-08 16:57:28 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import numpy as np
-import matplotlib.pyplot as plt
 from matplotlib.figure import Figure
 import matplotlib.patches as patches
 from mpl_toolkits.mplot3d import Axes3D
